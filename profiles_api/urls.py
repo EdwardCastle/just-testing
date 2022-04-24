9b2165d43a39c0b2396994474d075b8c2340810d
edwardcastle <sir.edwardcastle@gmail.com>
2022-04-23 22:50:52 -0400

    feat: Added login

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register('profile', views.UserProfileViewset)
 
 urlpatterns = [
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, filters
 from profiles_api import serializers, models, permisions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class UserProfileViewset(viewsets.ModelViewSet):
@@ -12,3 +14,8 @@ class UserProfileViewset(viewsets.ModelViewSet):
     permission_classes = (permisions.UpdateOnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Create authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
